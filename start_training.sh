52bbd0e2bce2b36a6cf35cedb9dca71509808bd1
Otnielush <Otniel.Elkayam@gmail.com>
2025-03-12 15:03:54 +0200

    bash script fix

--- start_training.sh
+++ start_training.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python otniel_scripts/caption_with_florence-2.py /dataset --output_dir /train_dataset
-accelerate launch --num_processes $GPU_NUM --mixed_precision bf16 --num_cpu_threads_per_process 2 run.py /workspace/config/train_config_$GPU_NUMh100.yaml
+accelerate launch --num_processes $GPU_NUM --mixed_precision bf16 --num_cpu_threads_per_process 2 run.py /workspace/config/train_config_${GPU_NUM}h100.yaml
